aa634631463b07146a4e27d2824f6d0b82ab7415
Deftu <deftudev@gmail.com>
2022-09-11 21:10:36 +0200

    Fixed crashes with potentially invalid SXCU files.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ pluginManagement {
         kotlin("jvm") version(kotlin)
         kotlin("plugin.serialization") version(kotlin)
 
-        val ucgt = "1.9.13"
+        val ucgt = "1.11.2-beta.10"
         id("xyz.unifycraft.gradle.multiversion-root") version(ucgt)
     }
 }
--- src/main/kotlin/xyz/deftu/screencapper/config/SxcuImport.kt
+++ src/main/kotlin/xyz/deftu/screencapper/config/SxcuImport.kt
@@ -3,9 +3,11 @@ package xyz.deftu.screencapper.config
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.annotations.SerializedName
+import org.apache.logging.log4j.LogManager
 import java.io.File
 
 object SxcuImport {
+    private val logger = LogManager.getLogger("SXCU Importer")
     private val gson = GsonBuilder()
         .setPrettyPrinting()
         .setLenient()
@@ -13,10 +15,12 @@ object SxcuImport {
 
     fun import(file: File) {
         val sxcu = gson.fromJson(file.readText(), Sxcu::class.java)
-        if (sxcu.version.substringBefore(".").toInt() < 13) throw IllegalStateException("sxcu version is too old. It must be version 13 or higher.")
-        if (!sxcu.destinationType.lowercase().contains("imageuploader")) throw IllegalStateException("sxcu destination type does not include images. This is required.")
+        if (sxcu.version != null) {
+            if (sxcu.version.substringBefore(".").toInt() < 13) throw IllegalStateException("SXCU version is too old. It must be version 13 or higher.")
+        } else logger.warn("Could not check SXCU config's version... This could cause problems!")
+        if (!sxcu.destinationType.lowercase().contains("imageuploader")) throw IllegalStateException("SXCU destination type does not include images. This is required.")
 
-        ScreencapperConfig.shareXRequestMethod = sxcu.requestMethod
+        ScreencapperConfig.shareXRequestMethod = sxcu.requestMethod ?: RequestMethod.POST
         ScreencapperConfig.shareXUploadUrl = sxcu.requestUrl
         ScreencapperConfig.markDirty()
         ScreencapperConfig.writeData()
@@ -31,9 +35,9 @@ object SxcuImport {
 }
 
 data class Sxcu(
-    @SerializedName("Version") val version: String,
+    @SerializedName("Version") val version: String?,
     @SerializedName("DestinationType") val destinationType: String,
-    @SerializedName("RequestMethod") val requestMethod: RequestMethod,
+    @SerializedName("RequestMethod") val requestMethod: RequestMethod?,
     @SerializedName("RequestURL") val requestUrl: String,
     @SerializedName("Headers") val headers: JsonObject?,
     @SerializedName("Arguments") val arguments: JsonObject?,
