f2a5f11b88b51fbe0beefef8ee11bbee6c8ac4c0
Deftu <deftudev@gmail.com>
2023-08-25 15:02:03 +0200

    Target all main methods in Main mixin

--- src/main/java/xyz/deftu/screencapper/mixins/MainMixin.java
+++ src/main/java/xyz/deftu/screencapper/mixins/MainMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin({Main.class})
 public class MainMixin {
 
-    @Inject(method = "main", at = @At("HEAD"), remap = false)
+    @Inject(method = "main*", at = @At("HEAD"), remap = false)
     private static void disableHeadlessMode(CallbackInfo ci) {
         if (!SystemUtils.IS_OS_MAC) System.setProperty("java.awt.headless", "false");
     }
