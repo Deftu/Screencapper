b92545cd218a7bf2ad86ef6b2302ca431726b33c
Deftu <deftudev@gmail.com>
2024-06-04 17:54:48 +0200

    Remove unused ChatHelper

--- src/main/kotlin/dev/deftu/screencapper/utils/ChatHelper.kt
+++
@@ -1,8 +0,0 @@
-package dev.deftu.screencapper.utils
-
-import net.minecraft.util.Formatting
-import dev.deftu.lib.utils.TextHelper
-
-object ChatHelper {
-    fun createLineDivider(number: Int) = TextHelper.createLiteralText("-".repeat(number)).formatted(Formatting.STRIKETHROUGH)
-}
