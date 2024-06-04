1eb5c0cde8ae6c708d7c706ccb75b701499236ce
Deftu <deftudev@gmail.com>
2024-06-04 17:43:37 +0200

    Redo build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,66 @@
+import com.modrinth.minotaur.dependencies.ModDependency
+import com.modrinth.minotaur.dependencies.DependencyType
+import dev.deftu.gradle.tools.minecraft.CurseRelation
+import dev.deftu.gradle.tools.minecraft.CurseRelationType
+import dev.deftu.gradle.utils.GameSide
+
 plugins {
-    id("xyz.deftu.gradle.multiversion-root")
+    java
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("dev.deftu.gradle.multiversion")
+    id("dev.deftu.gradle.tools")
+    id("dev.deftu.gradle.tools.shadow")
+    id("dev.deftu.gradle.tools.minecraft.loom")
+    id("dev.deftu.gradle.tools.minecraft.releases")
 }
 
-preprocess {
-    val fabric12001 = createNode("1.20.1-fabric", 1_20_01, "yarn")
-    val fabric11902 = createNode("1.19.2-fabric", 1_19_02, "yarn")
+toolkit.useDevAuth()
+toolkitLoomHelper.disableRunConfigs(GameSide.SERVER)
 
-    fabric12001.link(fabric11902)
+fun Dependency?.excludeVitals(): Dependency = apply {
+    check(this is ExternalModuleDependency)
+    exclude(module = "kotlin-stdlib")
+    exclude(module = "kotlin-stdlib-common")
+    exclude(module = "kotlin-stdlib-jdk8")
+    exclude(module = "kotlin-stdlib-jdk7")
+    exclude(module = "kotlin-reflect")
+    exclude(module = "annotations")
+    exclude(module = "fabric-loader")
+}!!
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.fabricApiVersion}")
+    modImplementation(mcData.modMenuDependency)
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.7.4+kotlin.1.6.21")
+    modImplementation("dev.deftu:DeftuLib-${mcData.versionStr}-${mcData.loader.name}:1.8.0")
+
+    api(include("ca.weblite:java-objc-bridge:1.2")!!)
+}
+
+toolkitReleases {
+    val versionChangelogFile = rootProject.file("changelogs/${modData.version}.md")
+    if (versionChangelogFile.exists()) changelogFile.set(versionChangelogFile)
+
+    modrinth {
+        projectId.set("yzNFLSqx")
+        dependencies.set(listOf(
+            ModDependency("P7dR8mSH", DependencyType.REQUIRED),                     // Fabric API
+            ModDependency("Ha28R6CL", DependencyType.REQUIRED),                     // Fabric Language Kotlin
+            ModDependency("mOgUt4GM", DependencyType.OPTIONAL),                     // Mod Menu
+            ModDependency("WfhjX9sQ", DependencyType.REQUIRED)                      // DeftuLib
+        ))
+    }
+
+    curseforge {
+        projectId.set("640948")
+        relations.set(listOf(
+            CurseRelation("fabric-api", CurseRelationType.REQUIRED),                // Fabric API
+            CurseRelation("fabric-language-kotlin", CurseRelationType.REQUIRED),    // Fabric Language Kotlin
+            CurseRelation("modmenu", CurseRelationType.OPTIONAL),                   // Mod Menu
+            CurseRelation("deftulib", CurseRelationType.REQUIRED)                   // DeftuLib
+        ))
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,12 +2,13 @@ import groovy.lang.MissingPropertyException
 
 pluginManagement {
     repositories {
-        // Default repositories
-        gradlePluginPortal()
-        mavenCentral()
+        // Snapshots
+        maven("https://maven.deftu.dev/snapshots")
+        maven("https://s01.oss.sonatype.org/content/groups/public/")
+        mavenLocal()
 
         // Repositories
-        maven("https://maven.deftu.xyz/releases")
+        maven("https://maven.deftu.dev/releases")
         maven("https://maven.fabricmc.net")
         maven("https://maven.architectury.dev/")
         maven("https://maven.minecraftforge.net")
@@ -15,10 +16,9 @@ pluginManagement {
         maven("https://server.bbkr.space/artifactory/libs-release/")
         maven("https://jitpack.io/")
 
-        // Snapshots
-        maven("https://maven.deftu.xyz/snapshots")
-        maven("https://s01.oss.sonatype.org/content/groups/public/")
-        mavenLocal()
+        // Default repositories
+        gradlePluginPortal()
+        mavenCentral()
     }
 
     plugins {
@@ -26,23 +26,27 @@ pluginManagement {
         kotlin("jvm") version(kotlin)
         kotlin("plugin.serialization") version(kotlin)
 
-        val dgt = "1.19.1"
-        id("xyz.deftu.gradle.multiversion-root") version(dgt)
+        val dgt = "1.26.0"
+        id("dev.deftu.gradle.multiversion-root") version(dgt)
     }
 }
 
-val projectName: String = extra["mod.name"]?.toString()
-    ?: throw MissingPropertyException("mod.name has not been set.")
+val projectName: String = extra["mod.name"]?.toString() ?: throw MissingPropertyException("mod.name has not been set.")
 rootProject.name = projectName
-rootProject.buildFileName = "build.gradle.kts"
+rootProject.buildFileName = "root.gradle.kts"
 
 listOf(
+    "1.18.2-fabric",
     "1.19.2-fabric",
-    "1.20.1-fabric"
+    "1.19.4-fabric",
+    "1.20.1-fabric",
+    "1.20.2-fabric",
+    "1.20.4-fabric",
+    "1.20.6-fabric"
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
         projectDir = file("versions/$version")
-        buildFileName = "../../version.gradle.kts"
+        buildFileName = "../../build.gradle.kts"
     }
 }
